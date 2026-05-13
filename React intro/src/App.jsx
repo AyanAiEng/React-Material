c996eeb28c6bbd64dca4f54e65496a3314464d91
Ayan Ali <braincoder.ai@gmail.com>
2026-05-13 12:41:14 +0500

    React

---
+++ React intro/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <p>My frist experience with react and i am very much excited</p>
+    </>
+  )
+}
+export default App
