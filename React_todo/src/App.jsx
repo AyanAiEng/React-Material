aa9313aad345fc77bc133369e9c338f49393d0f7
Ayan Ali <braincoder.ai@gmail.com>
2026-05-23 15:31:33 +0500

    React

---
+++ React_todo/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import Todo from './Components/todo'
+import './todo.css'
+function App() {
+  return(
+    <Todo />
+  )
+}
+export default App
---
+++ React_todo/src/Components/todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Todo() {
+  return (
+    <div className='container'>
+      <h1>Todo App</h1>
+      <div className="todo-input">
+        <input type="text" id='box' />
+        <button>Add</button>
+      </div>
+    </div>
+
+  )
+}
