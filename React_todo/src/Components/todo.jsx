28834f9e252041b1af55454a5e6de81de97d3e21
Ayan Ali <braincoder.ai@gmail.com>
2026-05-28 03:52:29 +0500

    React

--- React_todo/src/Components/todo.jsx
+++ React_todo/src/Components/todo.jsx
@@ -1,14 +1,70 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Todo() {
+  const [input, setinput] = useState("")
+const [todo, settodo] = useState([
+  {
+    value: "Mango",
+    iscompleted: false,
+  },
+  {
+    value: "Mango",
+    iscompleted: false,
+  },
+  {
+    value: "Mango",
+    iscompleted: false,
+  },
+  {
+    value: "Mango",
+    iscompleted: false,
+  },
+  {
+    value: "Mango",
+    iscompleted: false,
+  },
+  {
+    value: "Mango",
+    iscompleted: false,
+  },
+  {
+    value: "Mango",
+    iscompleted: false,
+  },
+  {
+    value: "Mango",
+    iscompleted: false,
+  }
+])
+   
+  let addtodo = () => {
+    console.log({ input })
+  }
+  let ontyping = (a) => {
+    console.log("Typing", a.target.value)
+    setinput(a.target.value)
+  }
   return (
-    <div className='container'>
-      <h1>Todo App</h1>
-      <div className="todo-input">
-        <input type="text" id='box' />
-        <button>Add</button>
+    <>
+      <div className='container'>
+        <h1>Todo App</h1>
+        <div className="todo-input">
+          <input onChange={ontyping} type="text" id='box' />
+          <button onClick={addtodo}>Add</button>
+        </div>
+        <ul>
+        {
+          todo.map((v,i) => {
+            return(
+              <li>hello</li>
+            )
+          })
+        }
+        </ul>
       </div>
-    </div>
+
+    </>
 
   )
 }
